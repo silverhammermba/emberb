84be392b3ac2c8c4e0ac404bda672531666a4549
Max <silverhammermba@gmail.com>
2014-10-19 18:50:45 -0400

    Add more advanced embedding example

---
+++ _includes/advanced.c
@@ -0,0 +1,18 @@
+#include <ruby.h>
+
+int main(int argc, char** argv)
+{
+	if (ruby_setup())
+	{
+		/* run code without Ruby */
+	}
+	else
+	{
+
+		/* Ruby goes here */
+
+		ruby_finalize(); // XXX you should probably guard against exceptions here!!!
+	}
+
+	return 0;
+}
