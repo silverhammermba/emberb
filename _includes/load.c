5bc663b8f1623a0feb2e8ffc96e4ea9546bea4e6
Max <silverhammermba@gmail.com>
2014-10-22 13:28:24 -0400

    Threading test, wording

--- _includes/load.c
+++ _includes/load.c
@@ -1,76 +1,23 @@
 #include <ruby.h>
 #include <stdio.h>
 
+VALUE my_thread(VALUE i)
+{
+	rb_thread_sleep(2);
+	printf("%d\n", FIX2INT(i));
+}
+
 int main(int argc, char* argv[])
 {
 	ruby_init();
 
-	VALUE cTest = rb_define_class("Test", rb_cObject);
+	VALUE threads[10];
 
-	VALUE x = rb_funcall(cTest, rb_intern("new"), 0);
+	for (int i = 0; i < 10; ++i)
+		threads[i] = rb_thread_create(RUBY_METHOD_FUNC(my_thread), (void*)INT2FIX(i));
 
-	switch (TYPE(x))
-	{
-		case T_NIL:
-			printf("nil\n");
-			break;
-		case T_CLASS:
-			printf("class\n");
-			break;
-		case T_MODULE:
-			printf("module\n");
-			break;
-		case T_FLOAT:
-			printf("float\n");
-			break;
-		case T_STRING:
-			printf("string\n");
-			break;
-		case T_REGEXP:
-			printf("regexp\n");
-			break;
-		case T_ARRAY:
-			printf("array\n");
-			break;
-		case T_HASH:
-			printf("hash\n");
-			break;
-		case T_STRUCT:
-			printf("struct\n");
-			break;
-		case T_BIGNUM:
-			printf("bignum\n");
-			break;
-		case T_FIXNUM:
-			printf("fixnum\n");
-			break;
-		case T_COMPLEX:
-			printf("complex\n");
-			break;
-		case T_RATIONAL:
-			printf("rational\n");
-			break;
-		case T_FILE:
-			printf("file\n");
-			break;
-		case T_TRUE:
-			printf("true\n");
-			break;
-		case T_FALSE:
-			printf("false\n");
-			break;
-		case T_DATA:
-			printf("data\n");
-			break;
-		case T_SYMBOL:
-			printf("symbol\n");
-			break;
-		case T_OBJECT:
-			printf("object\n");
-			break;
-		default:
-			printf("N/A\n");
-	}
+	for (int i = 0; i < 10; ++i)
+		rb_funcall(threads[i], rb_intern("join"), 0);
 
 	return ruby_cleanup(0);
 }
