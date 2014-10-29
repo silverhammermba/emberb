8297184bae7576e7067a86ded0e2f7902b8dd897
Max <silverhammermba@gmail.com>
2014-10-29 16:35:40 -0400

    Describe gotchas with NUM macros

--- _includes/load.c
+++ _includes/load.c
@@ -1,22 +1,14 @@
 #include <ruby.h>
-#include <ruby/thread.h>
-
-VALUE my_thread(VALUE arg)
-{
-	rb_thread_sleep(5);
-	return arg;
-}
+#include <limits.h>
 
 int main(int argc, char* argv[])
 {
 	ruby_init();
 
-	VALUE x = INT2FIX(10);
+	VALUE y = INT2FIX(INT_MAX);
+	char z = NUM2CHR(y);
 
-	VALUE thread = rb_thread_create(my_thread, (void*)x);
-
-	x = rb_funcall(thread, rb_intern("join"), 0);
-	rb_funcall(rb_mKernel, rb_intern("p"), 1, x);
+	printf("%hhd\n", z);
 
 	return ruby_cleanup(0);
 }
