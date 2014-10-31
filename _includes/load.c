ef328d2f1687e18353018ce2aadb15c252404101
Max <silverhammermba@gmail.com>
2014-10-31 17:07:46 -0400

    Verify that %i doesn't work at all

--- _includes/load.c
+++ _includes/load.c
@@ -1,45 +1,20 @@
 #include <ruby.h>
-#include <ruby/thread.h>
-#include <unistd.h>
 
-void* concurrent_func(void* arg)
+VALUE func(VALUE arg)
 {
-	unsigned int* x = arg;
-
-	for (unsigned int i = 0; i < *x; ++i)
-		sleep(1);
-
-	*x *= 2;
-
-	return NULL;
-}
-
-void unblock_func(void* arg)
-{
-	unsigned int* x = arg;
-	*x = 0;
-}
-
-VALUE foo(VALUE self, VALUE x)
-{
-	Check_Type(x, T_FIXNUM);
-
-	unsigned int y = NUM2UINT(x);
-
-	//concurrent_func((void*)&y);
-	rb_thread_call_without_gvl(concurrent_func, &y, unblock_func, &y);
-
-	return LONG2FIX(y);
+	VALUE str = rb_sprintf("%i %+i : %d %+d", arg, arg, arg, arg);
+	rb_funcall(rb_mKernel, rb_intern("puts"), 1, str);
+	rb_warn("%i %+i : %d %+d", arg, arg, arg, arg);
+	rb_raise(rb_eRuntimeError, "%i %+i : %d %+d", arg, arg, arg, arg);
 }
 
 int main(int argc, char* argv[])
 {
 	ruby_init();
 
-	rb_define_global_function("foo", foo, 1);
-
 	int state;
-	rb_eval_string_protect("threads = Array.new(3) { Thread.new { foo 10 } }; threads.map(&:kill)", &state);
+	VALUE x = INT2FIX(314);
+	rb_protect(func, x, &state);
 
 	return ruby_cleanup(state);
 }
