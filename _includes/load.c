5d54c7e034de2cf77eaab8d0f20152ef7203ec93
Max <silverhammermba@gmail.com>
2014-11-01 12:17:09 -0400

    The %i mystery has been solved

--- _includes/load.c
+++ _includes/load.c
@@ -2,10 +2,14 @@
 
 VALUE func(VALUE arg)
 {
-	VALUE str = rb_sprintf("%i %+i : %d %+d", arg, arg, arg, arg);
+VALUE x;
+x = rb_str_new_cstr("Hello, world!");
+
+VALUE str;
+str = rb_sprintf("pi = %f. %"PRIsVALUE" inspected: %+"PRIsVALUE, M_PI, x, x);
 	rb_funcall(rb_mKernel, rb_intern("puts"), 1, str);
-	rb_warn("%i %+i : %d %+d", arg, arg, arg, arg);
-	rb_raise(rb_eRuntimeError, "%i %+i : %d %+d", arg, arg, arg, arg);
+	rb_warn("%"PRIsVALUE" %+"PRIsVALUE" : %ld %+ld", arg, arg, arg, arg);
+	rb_raise(rb_eRuntimeError, "%"PRIsVALUE" %+"PRIsVALUE" : %ld %+ld", arg, arg, arg, arg);
 }
 
 int main(int argc, char* argv[])
@@ -13,7 +17,7 @@ int main(int argc, char* argv[])
 	ruby_init();
 
 	int state;
-	VALUE x = INT2FIX(314);
+	VALUE x = rb_eval_string_protect("\"hey\"", &state);
 	rb_protect(func, x, &state);
 
 	return ruby_cleanup(state);
