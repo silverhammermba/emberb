7e5508f0fe1bbd471f4b6c04cd4a71e9cb58f380
Max <silverhammermba@gmail.com>
2014-11-02 12:07:52 -0500

    Add section on require/load. Link to wrap bug

--- _test/load.c
+++ _test/load.c
@@ -1,29 +1,12 @@
 #include <ruby.h>
 
-void error(int state)
-{
-	if (!state) return;
-
-	VALUE exception = rb_errinfo();
-	rb_set_errinfo(Qnil);
-
-	rb_warn("%"PRIsVALUE, exception);
-}
-
 int main(int argc, char* argv[])
 {
 	ruby_init();
 
-	int state;
+	void* node = rb_load_file("-");
 
-	rb_eval_string_protect("def x; 3; end", &state);
-	error(state);
+	int state = ruby_exec_node(node);
 
-	rb_eval_string_wrap("puts x", &state);
-	error(state);
-
-	//rb_eval_string_protect("puts x", &state);
-	//error(state);
-
-	return ruby_cleanup(0);
+	return ruby_cleanup(state);
 }
