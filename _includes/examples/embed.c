e8ddd387bc6a303a8f082cc08edb5611971cd4ce
Max <silverhammermba@gmail.com>
2014-11-01 22:18:04 -0400

    Add API for AI. Fix file not closing.

--- _includes/examples/embed.c
+++ _includes/examples/embed.c
@@ -9,10 +9,10 @@
 #include <ruby.h>
 
 /* constants */
-const unsigned int actor_size = 30;
 const unsigned int win_width = 1024;
 const unsigned int win_height = 768;
-const char ai_script[] = "ai.rb";
+const unsigned int actor_size = 30;
+const char* ai_script = "ai.rb";
 
 /* globals */
 bool ai_loaded = false;
@@ -35,6 +35,16 @@ struct actor
 	SDL_Color color;
 };
 
+/*
+ * globals for stuff we want to access from Ruby methods. Yeah it's ugly but
+ * that's not the point of this example. If you wanted to do this properly, you
+ * would define a class wrapping the structs so that they can actually be
+ * passed to the methods
+ */
+struct actor* g_player;
+struct actor* g_enemy;
+
+/* for handling exceptions with protect calls */
 void handle_ruby_error()
 {
 	ai_error = true;
@@ -65,6 +75,7 @@ void update_ai(struct actor* act)
 		if (ai_loaded)
 			fprintf(stderr, "Can't stat AI script\n");
 		ai_loaded = false;
+		fclose(script);
 		return;
 	}
 
@@ -72,7 +83,10 @@ void update_ai(struct actor* act)
 	if (ai_loaded)
 	{
 		if (ai_load_time >= script_stat.st_mtime)
+		{
+			fclose(script);
 			return;
+		}
 	}
 	else
 		ai_loaded = true;
@@ -90,6 +104,7 @@ void update_ai(struct actor* act)
 	}
 	fread(buffer, 1, script_stat.st_size, script);
 	buffer[script_stat.st_size] = '\0';
+	fclose(script);
 
 	ai_error = false;
 	act->color.a = 255;
@@ -104,6 +119,7 @@ void update_ai(struct actor* act)
 		handle_ruby_error();
 }
 
+/* for rescuing exception in the AI script */
 VALUE think_wrapper(VALUE unused)
 {
 	rb_funcall(rb_mKernel, rb_intern("think"), 0);
@@ -111,6 +127,7 @@ VALUE think_wrapper(VALUE unused)
 	return Qundef;
 }
 
+/* run the AI script if possible */
 void ai_think(struct actor* act)
 {
 	/* indicate that AI isn't running */
@@ -158,6 +175,50 @@ void draw_actor(SDL_Renderer* renderer, struct actor* act)
 	SDL_RenderFillRect(renderer, &rectangle);
 }
 
+/* stuff we're defining for use in the AI script */
+VALUE m_time(VALUE self)
+{
+	return UINT2NUM(SDL_GetTicks());
+}
+
+VALUE m_player_pos(VALUE self)
+{
+	return rb_ary_new_from_args(2, INT2NUM(g_player->pos.x), INT2NUM(g_player->pos.y));
+}
+
+VALUE m_player_dir(VALUE self)
+{
+	return rb_ary_new_from_args(2, INT2NUM(g_player->dir.x), INT2NUM(g_player->dir.y));
+}
+
+VALUE m_pos(VALUE self)
+{
+	return rb_ary_new_from_args(2, INT2NUM(g_enemy->pos.x), INT2NUM(g_enemy->pos.y));
+}
+
+VALUE m_move(VALUE self, VALUE x, VALUE y)
+{
+	Check_Type(x, T_FIXNUM);
+	Check_Type(y, T_FIXNUM);
+
+	int nx = FIX2INT(x);
+	int ny = FIX2INT(y);
+
+	if (nx < -1)
+		nx = -1;
+	else if (nx > 1)
+		nx = 1;
+	if (ny < -1)
+		ny = -1;
+	else if (ny > 1)
+		ny = 1;
+
+	g_enemy->dir.x = nx;
+	g_enemy->dir.y = ny;
+
+	return Qnil;
+}
+
 int main(int argc, char** argv)
 {
 	/* start Ruby */
@@ -168,6 +229,13 @@ int main(int argc, char** argv)
 	}
 	/* set a nicer script name than <main> */
 	ruby_script("ruby");
+
+	/* define our own little API for use in the AI script */
+	rb_define_global_function("time", m_time, 0);
+	rb_define_global_function("player_pos", m_player_pos, 0);
+	rb_define_global_function("player_dir", m_player_dir, 0);
+	rb_define_global_function("pos", m_pos, 0);
+	rb_define_global_function("move", m_move, 2);
 
 	/* start SDL */
 	SDL_Init(SDL_INIT_VIDEO);
@@ -210,6 +278,10 @@ int main(int argc, char** argv)
 		.color = { .r = 255, .g = 0, .b = 0, .a = 255 }
 	};
 
+	/* ugly globals */
+	g_player = &player;
+	g_enemy = &enemy;
+
 	/* set up timing */
 	unsigned int frame_step = 16;
 	unsigned int ai_step = 33;
@@ -218,9 +290,11 @@ int main(int argc, char** argv)
 	unsigned int frame_time;
 	unsigned int ai_time;
 
+	/* start up AI */
 	update_ai(&enemy);
 	ai_think(&enemy);
 
+	/* for player input */
 	const Uint8* keyboard = SDL_GetKeyboardState(NULL);
 
 	/* main loop */
@@ -267,7 +341,7 @@ int main(int argc, char** argv)
 		/* do we need to check for a new AI script? */
 		bool ai_check_reload = true;
 
-		/* AI */
+		/* AI movement */
 		while (ai_time >= ai_step)
 		{
 			if (ai_check_reload)
@@ -298,6 +372,9 @@ int main(int argc, char** argv)
 		draw_actor(renderer, &player);
 
 		SDL_RenderPresent(renderer);
+
+		/* let CPU rest */
+		SDL_Delay(1);
 	}
 
 	/* clean up */
