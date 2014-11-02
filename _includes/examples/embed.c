3e73f925cbd1bb8c1599a1fef9556cd068b55474
Max <silverhammermba@gmail.com>
2014-11-01 20:00:50 -0400

    Add keyboard movement of player

--- _includes/examples/embed.c
+++ _includes/examples/embed.c
@@ -1,23 +1,52 @@
 #include <stdio.h>
 #include <stdbool.h>
+
 #include <SDL2/SDL.h>
 #include <ruby.h>
 
 const unsigned int actor_size = 30;
+/* window dimensions */
+const unsigned int win_width = 1024;
+const unsigned int win_height = 768;
 
+/* for position and direction */
 struct vec2
 {
 	int x;
 	int y;
 };
 
+/* for the player and their opponent */
 struct actor
 {
 	struct vec2 pos;
 	struct vec2 dir;
+	float speed;
 	SDL_Color color;
 };
 
+/* move actor after ms time has elapsed */
+void step_actor(struct actor* act, unsigned int ms)
+{
+	float norm = 1.f;
+	if (act->dir.x != 0 && act->dir.y != 0)
+		norm = 0.70710678118654752440f; /* 1/sqrt(2) */
+
+	act->pos.x += (int)(act->dir.x * act->speed * (float)ms * norm);
+	act->pos.y += (int)(act->dir.y * act->speed * (float)ms * norm);
+
+	// bound to screen
+	if (act->pos.x < 0)
+		act->pos.x = 0;
+	else if (act->pos.x > win_width - actor_size)
+		act->pos.x = win_width - actor_size;
+	if (act->pos.y < 0)
+		act->pos.y = 0;
+	else if (act->pos.y > win_height - actor_size)
+		act->pos.y = win_height - actor_size;
+}
+
+/* draw an actor as a colored box */
 void draw_actor(SDL_Renderer* renderer, struct actor* act)
 {
 	SDL_SetRenderDrawColor(renderer, act->color.r, act->color.g, act->color.b, act->color.a);
@@ -39,10 +68,6 @@ int main(int argc, char** argv)
 	/* start SDL */
 	SDL_Init(SDL_INIT_VIDEO);
 
-	/* window dimensions */
-	unsigned int win_width = 1024;
-	unsigned int win_height = 768;
-
 	/* create window */
 	SDL_Window* window = SDL_CreateWindow(
 		"Tag",
@@ -50,7 +75,7 @@ int main(int argc, char** argv)
 		SDL_WINDOWPOS_UNDEFINED,
 		win_width,
 		win_height,
-		SDL_WINDOW_OPENGL
+		0
 	);
 	if (window == NULL)
 	{
@@ -66,14 +91,17 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
+	/* create actors */
 	struct actor player = {
 		.pos = { .x = win_width / 2 + 100 - actor_size / 2, .y = win_height / 2 - actor_size / 2 },
 		.dir = { .x = 0, .y = 0 },
+		.speed = 0.5f,
 		.color = { .r = 0, .g = 0, .b = 255, .a = 255 }
 	};
 	struct actor enemy = {
 		.pos = { .x = win_width / 2 - 100 - actor_size / 2, .y = win_height / 2 - actor_size / 2 },
 		.dir = { .x = 0, .y = 0 },
+		.speed = 0.6f,
 		.color = { .r = 255, .g = 0, .b = 0, .a = 255 }
 	};
 
@@ -82,6 +110,8 @@ int main(int argc, char** argv)
 	unsigned int last_time = SDL_GetTicks();
 	unsigned int now;
 	unsigned int frame_time;
+
+	const Uint8* keyboard = SDL_GetKeyboardState(NULL);
 
 	/* main loop */
 	SDL_Event event;
@@ -102,14 +132,33 @@ int main(int argc, char** argv)
 					running = false;
 					break;
 				case SDL_KEYDOWN:
-				case SDL_KEYUP:
-					break;
+					if (event.key.keysym.sym == SDLK_ESCAPE)
+					{
+						running = false;
+						break;
+					}
 			}
 		}
+
+		/* player movement */
+		player.dir.x = 0;
+		player.dir.y = 0;
+
+		if (keyboard[SDL_SCANCODE_UP])
+			player.dir.y -= 1;
+		if (keyboard[SDL_SCANCODE_DOWN])
+			player.dir.y += 1;
+		if (keyboard[SDL_SCANCODE_LEFT])
+			player.dir.x -= 1;
+		if (keyboard[SDL_SCANCODE_RIGHT])
+			player.dir.x += 1;
 
 		/* game step */
 		while (frame_time >= time_step)
 		{
+			step_actor(&enemy, time_step);
+			step_actor(&player, time_step);
+
 			frame_time -= time_step;
 		}
 
