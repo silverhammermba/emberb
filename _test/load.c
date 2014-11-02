fa3f4f5de41fed1dedf67619cd33f8f4d5607492
Max <silverhammermba@gmail.com>
2014-11-02 09:13:03 -0500

    Clarify some eval behavior. Minor changes

--- _test/load.c
+++ _test/load.c
@@ -1,15 +1,13 @@
 #include <ruby.h>
 
-VALUE func(VALUE arg)
+void error(int state)
 {
-VALUE x;
-x = rb_str_new_cstr("Hello, world!");
+	if (!state) return;
 
-VALUE str;
-str = rb_sprintf("pi = %f. %"PRIsVALUE" inspected: %+"PRIsVALUE, M_PI, x, x);
-	rb_funcall(rb_mKernel, rb_intern("puts"), 1, str);
-	rb_warn("%"PRIsVALUE" %+"PRIsVALUE" : %ld %+ld", arg, arg, arg, arg);
-	rb_raise(rb_eRuntimeError, "%"PRIsVALUE" %+"PRIsVALUE" : %ld %+ld", arg, arg, arg, arg);
+	VALUE exception = rb_errinfo();
+	rb_set_errinfo(Qnil);
+
+	rb_warn("%"PRIsVALUE, exception);
 }
 
 int main(int argc, char* argv[])
@@ -17,8 +15,15 @@ int main(int argc, char* argv[])
 	ruby_init();
 
 	int state;
-	VALUE x = rb_eval_string_protect("\"hey\"", &state);
-	rb_protect(func, x, &state);
 
-	return ruby_cleanup(state);
+	rb_eval_string_protect("def x; 3; end", &state);
+	error(state);
+
+	rb_eval_string_wrap("puts x", &state);
+	error(state);
+
+	//rb_eval_string_protect("puts x", &state);
+	//error(state);
+
+	return ruby_cleanup(0);
 }
