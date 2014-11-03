d91e040fd3bf48d0f76437e9f22ccc471e3630ce
Max <silverhammermba@gmail.com>
2014-11-02 21:34:27 -0500

    Start second example section

--- _includes/examples/ext/extconf.rb
+++ _includes/examples/ext/extconf.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 require 'mkmf'
 
-raise "Can't find GMP lib" unless have_library 'gmp'
-raise "Can't find GMP header" unless have_header 'gmp.h'
-raise "Can't find string header" unless have_header 'string.h'
+raise "Can't find GMP lib"       unless have_library 'gmp'
+raise "Can't find GMP header"    unless have_header  'gmp.h'
+raise "Can't find string header" unless have_header  'string.h'
 
 create_makefile 'gmp'
