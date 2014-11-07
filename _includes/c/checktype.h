9cf26d3cab04e947680cf0de578f4d8311bfc5d1
Max <silverhammermba@gmail.com>
2014-11-06 23:00:32 -0500

    Found a few more type checking macros

--- _includes/c/checktype.h
+++ _includes/c/checktype.h
@@ -1,3 +1,9 @@
 Check_Type(obj, T_STRING); /* raises a TypeError if TYPE(obj) != T_STRING */
+RB_TYPE_P(obj, T_STRING);  /* same as TYPE(obj) == T_STRING */
+
+/* tests for specific types */
 FIXNUM_P(obj);             /* same as TYPE(obj) == T_FIXNUM */
+RB_FLOAT_TYPE_P(obj);      /* same as TYPE(obj) == T_FLOAT */
+SYMBOL_P(obj);             /* same as TYPE(obj) == T_SYMBOL */
 NIL_P(obj);                /* same as TYPE(obj) == T_NIL */
+RTEST(obj);                /* test if obj is "truthy" i.e. not nil or false */
