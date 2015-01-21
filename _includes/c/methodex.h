eb25c75e12ba9a302faeec2d076c6f8d5c82d4c7
Max <silverhammermba@gmail.com>
2015-01-21 11:39:48 -0500

    Correct notation: options hash->kwargs

--- _includes/c/methodex.h
+++ _includes/c/methodex.h
@@ -3,9 +3,7 @@ VALUE my_method(int argc, VALUE* argv, VALUE self)
 	/*
 	 * We want to define a method like
 	 *
-	 *     def my_method man1, opt1 = true, opt2 = false, *splat, man2, opts = {}, &blk
-	 *
-	 * Interestingly, that gives you a SyntaxError in Ruby. But the API can do it :D
+	 *     def my_method man1, opt1 = true, opt2 = false, *splat, man2, **opts, &blk
 	*/
 
 	VALUE man1, man2;
@@ -16,9 +14,10 @@ VALUE my_method(int argc, VALUE* argv, VALUE self)
 
 	rb_scan_args(argc, argv, "12*1:&", &man1, &opt1, &opt2, &splat, &man2, &opts, &blk);
 
-	// set default values for optional args that weren't passed
+	/* you must manually set the default values for optional arguments */
 	if (NIL_P(opt1)) opt1 = Qtrue;
 	if (NIL_P(opt2)) opt2 = Qfalse;
+	/* opts will be nil (rather than {}) if no keyword arguments were passed */
 	if (NIL_P(opts)) opts = rb_hash_new();
 
 	/* ... */
