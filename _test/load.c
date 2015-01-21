3b061fd71ff81becb5a21d79a0dd0702f0165994
Max <silverhammermba@gmail.com>
2015-01-21 11:39:22 -0500

    Update Ruby version, testing method defs

--- _test/load.c
+++ _test/load.c
@@ -1,19 +1,35 @@
 #include <ruby.h>
+#include <stdio.h>
 
-VALUE test(VALUE x)
+VALUE test(int argc, VALUE* argv, VALUE self)
 {
-	VALUE y = rb_str_new_cstr("foo");
-	char z = NUM2CHR(y);
-	printf("%hhd\n", z);
-	return Qundef;
+	VALUE man1, man2;
+	VALUE opt1, opt2;
+	VALUE splat;
+	VALUE opts;
+	VALUE blk;
+
+	rb_scan_args(argc, argv, "12*1:&", &man1, &opt1, &opt2, &splat, &man2, &opts, &blk);
+
+	VALUE str;
+	str = rb_sprintf("%+"PRIsVALUE" %+"PRIsVALUE" %+"PRIsVALUE" %+"PRIsVALUE" %+"PRIsVALUE, opt1, opt2, splat, opts, blk);
+	printf("%s\n", StringValueCStr(str));
+
+	return Qnil;
 }
 
 int main(int argc, char* argv[])
 {
 	ruby_init();
 
+	rb_define_global_function("test", test, -1);
 	int state;
-	rb_protect(test, Qundef, &state);
+	rb_eval_string_protect("test 1, 2", &state);
+	rb_eval_string_protect("test 1, 2, 3", &state);
+	rb_eval_string_protect("test 1, 2, 3, 4", &state);
+	rb_eval_string_protect("test 1, 2, 3, 4, 5", &state);
+	rb_eval_string_protect("test 1, 2, 3, 4, 5, 6", &state);
+	rb_eval_string_protect("test 1, 2, 3, 4, 5, 6, a: 3", &state);
 
 	return ruby_cleanup(state);
 }
