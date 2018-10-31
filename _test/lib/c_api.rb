e88cdffeea6e1d357f7afe700504ccc9c4b14271
Max <silverhammermba@gmail.com>
2018-10-31 18:20:05 -0400

    add excpetion test

--- _test/lib/c_api.rb
+++ _test/lib/c_api.rb
@@ -4,9 +4,21 @@ require 'tempfile'
 RUBY_VERSION = "2.5"
 
 module CAPI
+  class Result
+    def initialize out, err, status
+      @out = out
+      @err = err
+      @status = status
+    end
+
+    attr_reader :out, :err, :status
+  end
+
   class CompilerError < StandardError; end
 
-  def self.run_c code
+  # run a block of C code with the VM already initalized, must include an entry point:
+  # void ruby_main(void)
+  def self.run_c_blocks code
     code = <<-SOURCE
       #include <ruby.h>
       #include <stdio.h>
@@ -35,16 +47,21 @@ module CAPI
       end
 
       puts "Running:"
-      out, err, status = Open3.capture3(exe.path)
+      result = Result.new *Open3.capture3(exe.path)
       # TODO what does a segfault look like?
       puts out
       puts err
       p status
 
-      return out
+      return result
     ensure
       exe.unlink
     end
+  end
+
+  # run C statements with the Ruby VM initialized, in the context of a top-level function
+  def self.run_c source
+    run_c_blocks "void ruby_main() { #{source}; }"
   end
 
   # TODO would also be cool to have a method that compiles a lib and then requires it
--- _test/spec/c_api_spec.rb
+++ _test/spec/c_api_spec.rb
@@ -3,9 +3,13 @@ require 'c_api'
 include RSpec
 
 describe CAPI do
-  context "when doing nothing" do
-    it "runs fine" do
-      expect(CAPI.run_c("void ruby_main() rb_eval_string_protect(\"puts 'Hello, world!'\", NULL);}")).to eq ""
+  context "when not testing API functions" do
+    it "runs fine with no input" do
+      expect(CAPI.run_c("").out).to eq ""
+    end
+
+    it "raises on invalid input" do
+      expect { CAPI.run_c("{") }.to raise_error CAPI::CompilerError
     end
   end
 end
