f532014c31de5cb99e6e4a9e8f0984b6b4558722
Max <silverhammermba@gmail.com>
2018-10-31 23:40:12 -0400

    clean up tests a bit, one still failing

--- _test/spec/c_api_spec.rb
+++ _test/spec/c_api_spec.rb
@@ -3,12 +3,12 @@ require 'c_api'
 include RSpec
 
 describe CAPI do
-  context "when not testing API functions" do
-    it "VM works" do
+  describe "setup" do
+    it "works with no code" do
       expect(CAPI.run_c("").out).to eq ""
     end
 
-    it "VM works with setup/finalize" do
+    it "works with setup/finalize" do
       expect(CAPI.run_c_main(<<-SOURCE
         int main(int argc, char* argv[])
         {
@@ -27,12 +27,36 @@ describe CAPI do
       ).out).to eq ""
     end
 
-    it "invalid input raises" do
+    it "raises when code is invalid" do
       expect { CAPI.run_c("{") }.to raise_error CAPI::CompilerError
     end
 
-    it "runtime errors raise" do
+    it "raises when code causes segfault" do
       expect { CAPI.run_c("ruby_cleanup(0)") }.to raise_error CAPI::RuntimeError
+    end
+  end
+
+  describe "eval" do
+    it "works with puts" do
+      expect(CAPI.run_c(<<-SOURCE
+        int state;
+        rb_eval_string_protect("puts 'Hello, world!'", &state);
+
+        if (state) { printf("Error"); }
+        SOURCE
+      ).out).to eq "Hello, world!\n"
+    end
+
+    it "fails with invalid code" do
+      message = "Failed in a good way"
+
+      expect(CAPI.run_c(<<-SOURCE
+        int state;
+        rb_eval_string_protect("foobar", &state);
+
+        if (state) { printf("#{message}"); }
+        SOURCE
+      ).out).to eq message
     end
   end
 end
